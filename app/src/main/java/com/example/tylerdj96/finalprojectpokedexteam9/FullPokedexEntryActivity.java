71e66a69e827644a6db4f932311f16dc41a13f80
tjones0517 <jonesty@oregonstate.edu>
2018-03-14 08:38:11 -0700

    got all pokedex regions added to settings

--- app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/FullPokedexEntryActivity.java
+++ app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/FullPokedexEntryActivity.java
@@ -1,7 +1,10 @@
 package com.example.tylerdj96.finalprojectpokedexteam9;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.tylerdj96.finalprojectpokedexteam9.utils.PokemonUtils;
 
 public class FullPokedexEntryActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_pokedex_entry);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(PokemonUtils.EXTRA_SEARCH_RESULT)) {
+            mForecastItem = (PokemonUtils.SearchResult)intent.getSerializableExtra(
+                    PokemonUtils.EXTRA_SEARCH_RESULT
+            );
+            fillInLayoutText(mForecastItem);
+        }
     }
 }
