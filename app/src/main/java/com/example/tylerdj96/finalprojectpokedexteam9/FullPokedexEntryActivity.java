8fa3925bc145c25a5ad68e11ffe46d3efbc150dc
tjones0517 <jonesty@oregonstate.edu>
2018-03-22 21:55:14 -0700

    improving search results

--- app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/FullPokedexEntryActivity.java
+++ app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/FullPokedexEntryActivity.java
@@ -1,12 +1,17 @@
 package com.example.tylerdj96.finalprojectpokedexteam9;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.graphics.Bitmap;
@@ -19,8 +24,11 @@ import com.example.tylerdj96.finalprojectpokedexteam9.utils.PokemonUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class FullPokedexEntryActivity extends AppCompatActivity {
+
+    private final static String TAG = "fullpokedexentryactivity";
 
     private TextView mTVEntryName;
     private ImageView mIVEntryImage;
@@ -29,26 +37,28 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
     private PokemonUtils.SearchResult mSearchResult;
     private PokemonUtils.DetailResult mDetailResult;
 
+    private Bitmap bitmap;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_pokedex_entry);
 
-        mTVEntryName = (TextView)findViewById(R.id.tv_entry_name);
-        mIVEntryImage = (ImageView)findViewById(R.id.iv_entry_image);
-        mTVEntryDescription = (TextView)findViewById(R.id.tv_entry_description);
+        mTVEntryName = (TextView) findViewById(R.id.tv_entry_name);
+        mIVEntryImage = (ImageView) findViewById(R.id.iv_entry_image);
+        mTVEntryDescription = (TextView) findViewById(R.id.tv_entry_description);
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(PokemonUtils.EXTRA_SEARCH_RESULT)) {
             mSearchResult = (PokemonUtils.SearchResult) intent.getSerializableExtra(PokemonUtils.EXTRA_SEARCH_RESULT);
-            try {
+            /*try {
                 mDetailResult = (PokemonUtils.DetailResult)PokemonUtils.parseDetailResultJson(NetworkUtils.doHTTPGet("https://pokeapi.co/api/v2/pokemon"+mSearchResult.name));
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            mTVEntryName.setText(mDetailResult.name);
+            }*/
+            //mTVEntryName.setText(mDetailResult.name);
 
-            Bitmap bitmap = null;
-            try {
+            bitmap = null;
+            /*try {
                 bitmap = BitmapFactory.decodeStream((InputStream)new URL(mDetailResult.sprite).getContent());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -56,7 +66,14 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
             mIVEntryImage.setImageBitmap(bitmap);
             mTVEntryDescription.setText("Type: "+mDetailResult.type+"\n"+
                     "Height: "+mDetailResult.height+"   Weight:"+mDetailResult.weight+"\n");
+            */
         }
+        Log.d(TAG, mSearchResult.name);
+
+        String query = Integer.toString(mSearchResult.entry_number);
+
+        PokemonDetailsSearch(query);
+
     }
 
     @Override
@@ -65,7 +82,7 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_view_pokemon:
@@ -74,11 +91,11 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
+    }*/
 
     public void viewPokemonOnWeb() {
         if (mDetailResult != null) {
-            Uri bulbapediaURL = Uri.parse("https://bulbapedia.bulbagarden.net/wiki/"+mDetailResult.name);
+            Uri bulbapediaURL = Uri.parse("https://bulbapedia.bulbagarden.net/wiki/" + mDetailResult.name);
             Intent webIntent = new Intent(Intent.ACTION_VIEW, bulbapediaURL);
             if (webIntent.resolveActivity(getPackageManager()) != null) {
                 startActivity(webIntent);
@@ -86,6 +103,61 @@ public class FullPokedexEntryActivity extends AppCompatActivity {
         }
     }
 
+    private void PokemonDetailsSearch(String searchQuery) {
+        //String githubSearchURL = PokemonUtils.buildGitHubSearchURL(searchQuery);
+        //Log.d(TAG, "querying search URL: " + githubSearchURL);
+        String detailsURL = "https://pokeapi.co/api/v2/pokemon/" + searchQuery;
+        Log.d(TAG, detailsURL);
+        new PokemonDetailTask().execute(detailsURL);
+    }
 
 
+    public class PokemonDetailTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //mLoadingProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String githubSearchURL = urls[0];
+
+            String searchResults = null;
+            try {
+                searchResults = NetworkUtils.doHTTPGet(githubSearchURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return searchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //mLoadingProgressBar.setVisibility(View.INVISIBLE);
+            Log.d(TAG, s);
+            if (s != null) {
+                PokemonUtils.DetailResult DetailsList = PokemonUtils.parseDetailResultJson(s);
+                mTVEntryName.setText(DetailsList.name);
+                try {
+                    bitmap = BitmapFactory.decodeStream((InputStream) new URL(DetailsList.sprite).getContent());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mIVEntryImage.setImageBitmap(bitmap);
+                mTVEntryDescription.setText("Type: " + DetailsList.type + "\n" +
+                        "Height: " + DetailsList.height + "   Weight:" + DetailsList.weight + "\n");
+            }
+            //mGitHubSearchAdapter.updateSearchResults(searchResultsList);
+            //mLoadingErrorMessage.setVisibility(View.INVISIBLE);
+            //mSearchResultsRV.setVisibility(View.VISIBLE);
+            else {
+                //mSearchResultsRV.setVisibility(View.INVISIBLE);
+                //mLoadingErrorMessage.setVisibility(View.VISIBLE);
+            }
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/utils/PokemonUtils.java
+++ app/src/main/java/com/example/tylerdj96/finalprojectpokedexteam9/utils/PokemonUtils.java
@@ -4,6 +4,8 @@ import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.example.tylerdj96.finalprojectpokedexteam9.MainActivity;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,6 +21,8 @@ import static android.content.ContentValues.TAG;
 
 public class PokemonUtils {
     public static final String EXTRA_SEARCH_RESULT = "PokemonUtils.SearchResult";
+
+    private final static String TAG = "pokemonutils";
 
     final static String POKEDEX_BASE_URL = "https://pokeapi.co/api/v2/pokedex/";
     final static String GITHUB_SEARCH_QUERY_PARAM = "q";
@@ -51,7 +55,14 @@ public class PokemonUtils {
             entry.type = detailResultObj.getJSONArray("types").getJSONObject(1).getString("name")+" "+
                     detailResultObj.getJSONArray("types").getJSONObject(0).getString("name");
             entry.sprite = detailResultObj.getJSONObject("sprites").getString("front_default");
-
+            Log.d(TAG, entry.name);
+            Log.d(TAG, Integer.toString(entry.id));
+            Log.d(TAG, Integer.toString(entry.weight));
+            Log.d(TAG, Integer.toString(entry.height));
+            //Log.d(TAG, entry.weight);
+            //rLog.d(TAG, entry.height);
+            Log.d(TAG, entry.type);
+            Log.d(TAG, entry.sprite);
             return entry;
         } catch (JSONException e) {
             return null;
@@ -74,8 +85,8 @@ public class PokemonUtils {
                 result.name = resultItemInner.getString("name");
                 result.pokemonURL = resultItemInner.getString("url");
 
-                Log.d(TAG, result.name);
-                Log.d(TAG, searchcriteria);
+                //Log.d(TAG, result.name);
+                //Log.d(TAG, searchcriteria);
 
                 if(result.name.contains(searchcriteria)) {
                     result.name = result.name.substring(0, 1).toUpperCase() + result.name.substring(1);
